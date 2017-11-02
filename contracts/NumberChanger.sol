3e361908d6649085a98701ee1545bb798e6e7a9b
Jack Tanner <ugmo04@users.noreply.github.com>
2017-11-01 22:05:39 -0500

    Upgradable methods are working as desired

--- contracts/NumberChanger.sol
+++ contracts/NumberChanger.sol
@@ -14,27 +14,56 @@ contract NumberChanger {
 
 contract UpgradableNumberChanger_Logic is Upgradable {
 
-  // No state information here at all
-
   UpgradableNumberChanger_State public state;
 
-  function UpgradableNumberChanger_Logic() {
+  function UpgradableNumberChanger_Logic() public {
     state = new UpgradableNumberChanger_State();
   }
 
-  function changeNumber() public {
+  function changeNumber() upgradeable public {
     state.setMyNumber(state.myNumber() + 1);
+  }
+
+  function getNumber() constant public returns (uint) {
+      return state.myNumber();
+  }
+
+  function upgradeContract(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    // TODO: check that newOwner is a contract...
+    state.transferOwnership(newOwner);
+    upgrade(newOwner);
+  }
+}
+
+contract NewUpgradableNumberChanger_Logic is Upgradable {
+
+  UpgradableNumberChanger_State public state;
+
+  function NewUpgradableNumberChanger_Logic(address _state) public {
+    state = UpgradableNumberChanger_State(_state);
+  }
+
+  function changeNumber() upgradeable public {
+    state.setMyNumber(state.myNumber() + 2); // We have updated the logic of the contract
+  }
+
+  function getNumber() constant public returns (uint) {
+      return state.myNumber();
   }
 
   function transferStateOwnership(address newOwner) onlyOwner public {
     require(newOwner != address(0));
+    // TODO: check that newOwner is a contract...
     state.transferOwnership(newOwner);
+    upgrade(newOwner);
   }
 }
 
 contract UpgradableNumberChanger_State is Ownable {
 
   // All state is in separate contract with setters and getters for ALL functions
+  // All setter functions have onlyOwner modifier
 
   uint public myNumber;
 
@@ -43,7 +72,7 @@ contract UpgradableNumberChanger_State is Ownable {
   }
 
   function kill() onlyOwner public {
-    suicide(owner);
+    selfdestruct(owner);
   }
 
 }
--- contracts/Upgradable.sol
+++ contracts/Upgradable.sol
@@ -4,34 +4,44 @@ import './lib/Ownable.sol';
 
 /**
  * @title Upgradable
- * @dev Contracts inheriting from Upgradeable can have their contract function calls routed to a new contract
- * if the contract is upgraded
+ * @dev Contracts inheriting from Upgradeable can have their contract function calls routed to a new contract when required
+ * Contracts inheriting Upgradable should separate contract logic from data and data update logic so that an
+ * upgraded contract can still use and update the old data state of the contract
+ * The new upgraded deployed logic contract must have the same interface, but can add new interfaces
  */
 contract Upgradable is Ownable {
 
-  address public upgradedContract;
+  address public upgraded;
+
+  event upgraded(address oldContract, address newContract);
 
   function Upgradable() public {
-    upgradedContract = address(this);
+    upgraded = address(this);
   }
 
   /**
-   * Uses the current contract function if no upgrade has been set (through ENS)
-   * If an upgrade has been set, send the call to the new function
-   * The upgraded function must have the same interface
-   * Once upgraded, only the upgraded contract can access the old contract's methods
+   * @dev Modifier that will use route the function to the upgraded function if
+   * the contract has been upgraded
    */
    modifier upgradeable () {
 
-       if (upgradedContract != address(this)) {
-           upgradedContract.delegatecall(msg.data); //TODO: ensure that gas and Ether are passed on as well
+       if (upgraded != address(this)) {
+           upgraded.delegatecall(msg.data); //TODO: ensure that gas and Ether are passed on as well
+           // TODO: how well will this work if upgraded contracts use different compilers???
        } else {
            // Execute as normal
            _;
        }
    }
 
-   function upgradeContract(address _upgradedContract) public {
-     upgradedContract = _upgradedContract;
+   /**
+    * @dev Updates the pointer to the contract logic pointer to the new upgraed contract
+    *
+    */
+   function upgrade(address _upgraded) onlyOwner internal {
+     require(_upgraded != address(0));
+     // TODO: check that _upgradedContract is a contract...
+     upgraded(upgraded, _upgraded);
+     upgraded = _upgraded;
    }
 }
